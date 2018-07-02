9e9f5f27c6bb92e5eb7363f05abb90cc705eb7aa
elayaraja89 <elayaraja.r@cm-excellence.com>
2018-07-02 19:50:18 +0530

    Update AbstractBean.java

--- JPetStore/src/com/ibatis/jpetstore/presentation/AbstractBean.java
+++ JPetStore/src/com/ibatis/jpetstore/presentation/AbstractBean.java
@@ -4,7 +4,7 @@ import org.apache.struts.beanaction.ActionContext;
 import org.apache.struts.beanaction.BaseBean;
 
 public abstract class AbstractBean extends BaseBean {
-
+//test
   public static final String SUCCESS = "success";
   public static final String FAILURE = "failure";
   public static final String SIGNON = "signon";
